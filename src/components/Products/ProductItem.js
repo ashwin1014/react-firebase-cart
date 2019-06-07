e4d6d32d7a096cbb1ef0e045836c7d5148677b29
ashwin1014 <ashwin1014@yahoo.co.in>
2019-06-08 01:59:12 +0530

    added cart notification

--- src/components/Products/ProductItem.js
+++ src/components/Products/ProductItem.js
@@ -1,30 +1,42 @@
 import React from 'react';
-import { Card, Icon, Button, Col } from 'antd';
+import { Card, Icon, Button, Col, Tooltip, Typography, notification } from 'antd';
 
 const { Meta } = Card;
+const { Paragraph } = Typography;
+
 
 const ProductItem = (props) => {
-     const {category, details, image, isFavourite, name, price, rating, reviews} = props.item
+    const {category, details, image, isFavourite, name, price, rating, reviews} = props.item
+
+    const handleCart = () => {
+        const args = {
+          message: `${name} added to cart`,
+          duration: 2,
+        };
+        notification.open(args);
+      };
+
     return (
         <Col lg={{ span: 8}} sm={{ span: 12}} style={{ marginBottom: 30 }}>
             <Card
-            style={{ width: 300 }}
-            cover={<img alt="food-item" src={image} />}
-            actions={[<p>&#8377; {price}</p>,<Button>Add to cart</Button>]}
+                hoverable
+                style={{ width: 300 }}
+                cover={<img alt="food-item" src={image} />}
+                actions={[<p>&#8377; {price}</p>,<Button onClick={handleCart}>Add to cart</Button>]}
             >
+            <Tooltip placement="top" title={name}>
             <Meta
-                title={name}
-                description={details}
+              title={name}
             />
-
+            </Tooltip>           
+            <Paragraph ellipsis={{ rows: 3, expandable: true }}>{details}</Paragraph>
             <div className="other-details">
             <p>Rating: {rating} <Icon type="star" theme="filled" />({reviews} Reviews)</p>
             <br/>
-            {isFavourite ? <Icon type="star" theme="filled" style={{color: 'yellow'}}/>:<Icon type="star" />}
+            {isFavourite ? <Icon type="star" theme="filled" style={{color: 'yellow', fontSize: 20}}/>:<Icon type="star" style={{fontSize: 20}}/>}
             <br/>
             Category: {category}
-           </div>
-            
+           </div>            
           </Card>   
         </Col>
     )
